a033d9ba562f19ba979a5b1fa1a1928704904761
L1558510748 <1558510748@qq.com>
2020-07-22 19:29:19 +0800

    cancel lazy

--- shuqi_cms/src/App.js
+++ shuqi_cms/src/App.js
@@ -1,10 +1,10 @@
 import React, { Fragment, lazy } from 'react';
 import './App.css';
 import { withRouter, Route, Switch } from "react-router-dom"
-// import Login from "./pages/Login/index"
-// import Index from "./pages/Index/index"
-const Login = lazy(() => import("./pages/Login"))
-const Index = lazy(() => import("./pages/Index"))
+import Login from "./pages/Login/index"
+import Index from "./pages/Index/index"
+// const Login = lazy(() => import("./pages/Login"))
+// const Index = lazy(() => import("./pages/Index"))
 class App extends React.Component {
   constructor(props) {
     super(props)
