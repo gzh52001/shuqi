77699e45bf1fb8476d8b81ca78c023096604e5c9
L1558510748 <1558510748@qq.com>
2020-07-24 11:25:36 +0800

    lazy

--- shuqi_cms/src/App.js
+++ shuqi_cms/src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment, lazy } from 'react';
+import React, { Fragment, lazy, Suspense } from 'react';
 import './App.css';
 import { withRouter, Route, Switch } from "react-router-dom"
-import Login from "./pages/Login/index"
-import Index from "./pages/Index/index"
-// const Login = lazy(() => import("./pages/Login"))
-// const Index = lazy(() => import("./pages/Index"))
+// import Login from "./pages/Login/index"
+// import Index from "./pages/Index/index"
+const Login = lazy(() => import("./pages/Login"))
+const Index = lazy(() => import("./pages/Index"))
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -12,11 +12,13 @@ class App extends React.Component {
   render() {
     return (
       <Fragment>
-        <Switch>
-          <Route path="/index" component={Index}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/" component={Index}></Route>
-        </Switch>
+        <Suspense fallback={<div>loading</div>}>
+          <Switch>
+            <Route path="/index" component={Index}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/" component={Index}></Route>
+          </Switch>
+        </Suspense>
       </Fragment>
     )
   }
